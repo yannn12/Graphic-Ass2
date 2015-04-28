7ace06765500c570534f78e4ee04322b954fb3b2
yannn12 <yannn12@gmail.com>
2015-04-28 15:46:47 +0300

    fix lightspot

--- Project5/SpotLight.cpp
+++ Project5/SpotLight.cpp
@@ -1,16 +1,16 @@
 #include "SpotLight.h"
 
-
 SpotLight::SpotLight(Vector3f& Location,Vector3f& Direction,Vector3f& Icolor,float RadAngel): Location(Location),Direction(Direction),Icolor(Icolor)
 {
 	this->Angel = cos(RadAngel);
-	Direction.normalize();
+	this->Direction.normalize();
 }
 
 
 SpotLight::~SpotLight(void)
 {
 }
+
 
 
 LightImpact SpotLight::lightImpact(Vector3f& point){
@@ -20,7 +20,8 @@ LightImpact SpotLight::lightImpact(Vector3f& point){
 	ImpactDir.normalize();
 	
 	float angel =  ImpactDir*(Direction);
-	
+
+
 	if(angel < this->Angel){
 		return LightImpact(); // no hit
 		 
